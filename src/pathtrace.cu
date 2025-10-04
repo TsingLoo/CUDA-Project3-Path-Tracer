1383f46c7f6130bcd45af8f56a8c31f61514457e
Xiaonan <tsingloo@outlook.com>
2025-10-04 10:42:41 -0400

    rename shadeFakeMaterial

--- src/pathtrace.cu
+++ src/pathtrace.cu
@@ -235,7 +235,7 @@ __global__ void computeIntersections(
 // Note that this shader does NOT do a BSDF evaluation!
 // Your shaders should handle that - this can allow techniques such as
 // bump mapping.
-__global__ void shadeFakeMaterial(
+__global__ void kernShadeMaterial(
     int iter,
     int num_paths,
     ShadeableIntersection* shadeableIntersections,
@@ -245,29 +245,32 @@ __global__ void shadeFakeMaterial(
     int idx = blockIdx.x * blockDim.x + threadIdx.x;
     if (idx < num_paths)
     {
+        if (pathSegments[idx].remainingBounces <= 0) {
+            return;
+        }
+
         ShadeableIntersection intersection = shadeableIntersections[idx];
         if (intersection.t > 0.0f) // if the intersection exists...
         {
-          // Set up the RNG
-          // LOOK: this is how you use thrust's RNG! Please look at
-          // makeSeededRandomEngine as well.
-            thrust::default_random_engine rng = makeSeededRandomEngine(iter, idx, 0);
-            thrust::uniform_real_distribution<float> u01(0, 1);
-
             Material material = materials[intersection.materialId];
             glm::vec3 materialColor = material.color;
 
             // If the material indicates that the object was a light, "light" the ray
             if (material.emittance > 0.0f) {
                 pathSegments[idx].color *= (materialColor * material.emittance);
+                pathSegments[idx].remainingBounces = 0;
             }
             // Otherwise, do some pseudo-lighting computation. This is actually more
             // like what you would expect from shading in a rasterizer like OpenGL.
             // TODO: replace this! you should be able to start with basically a one-liner
             else {
-                float lightTerm = glm::dot(intersection.surfaceNormal, glm::vec3(0.0f, 1.0f, 0.0f));
-                pathSegments[idx].color *= (materialColor * lightTerm) * 0.3f + ((1.0f - intersection.t * 0.02f) * materialColor) * 0.7f;
-                pathSegments[idx].color *= u01(rng); // apply some noise because why not
+                thrust::default_random_engine rng = makeSeededRandomEngine(iter, idx, 0);
+                thrust::uniform_real_distribution<float> u01(0, 1);
+
+                Ray& ray = pathSegments[idx].ray;
+
+                glm::vec3 intersect = ray.origin + ray.direction * intersection.t;
+                scatterRay(pathSegments[idx], intersect, intersection.surfaceNormal, material, rng);
             }
             // If there was no intersection, color the ray black.
             // Lots of renderers use 4 channel color, RGBA, where A = alpha, often
@@ -276,6 +279,7 @@ __global__ void shadeFakeMaterial(
         }
         else {
             pathSegments[idx].color = glm::vec3(0.0f);
+            pathSegments[idx].remainingBounces = 0;
         }
     }
 }
@@ -381,7 +385,7 @@ void pathtrace(uchar4* pbo, int frame, int iter)
         // TODO: compare between directly shading the path segments and shading
         // path segments that have been reshuffled to be contiguous in memory.
 
-        shadeFakeMaterial<<<numblocksPathSegmentTracing, blockSize1d>>>(
+        kernShadeMaterial<<<numblocksPathSegmentTracing, blockSize1d>>>(
             iter,
             num_paths,
             dev_intersections,
--- src/utilities.h
+++ src/utilities.h
@@ -38,6 +38,81 @@ inline __host__ __device__ bool solveQuadratic(float A, float B, float C, float&
     return true;
 }
 
+inline __device__ glm::vec2 squareToDiskConcentric(glm::vec2 xi)
+{
+    float a = 2.0 * xi.x - 1.0;
+    float b = 2.0 * xi.y - 1.0;
+
+    if (a == 0.0 && b == 0.0)
+    {
+        return glm::vec2(0.0);
+    }
+
+    float r, phi;
+
+    if (abs(a) > abs(b))
+    {
+        r = a;
+        phi = (PI / 4.0) * (b / a);
+    }
+    else
+    {
+        r = b;
+        phi = (PI / 2.0) - (a / b) * (PI / 4.0);
+    }
+
+    float x = r * cos(phi);
+    float y = r * sin(phi);
+    return glm::vec2(x, y);
+}
+
+inline __device__ glm::vec3 squareToHemisphereCosine(glm::vec2 xi)
+{
+    glm::vec2 disk = squareToDiskConcentric(xi);
+    float r2 = glm::dot(disk, disk);
+    float z = glm::sqrt(glm::max(0.0, 1.0 - r2));
+    return glm::vec3(disk.x, disk.y, z);
+}
+
+/// <summary>
+/// build a tangent space given a normal vector
+/// </summary>
+/// <param name="nor">the z-axis of the space</param>
+/// <param name="v2">the tangent of the space, x-axis</param>
+/// <param name="v3"></param>
+/// <returns></returns>
+inline __device__ void buildTangentSpace(const glm::vec3 nor, glm::vec3& v2, glm::vec3& v3)
+{
+    if (abs(nor.x) > abs(nor.y))
+        //cross(v1, vec3(0, 1, 0)) = vec3(-v1.z, 0, v1.x)
+        v2 = glm::vec3(-nor.z, 0, nor.x) / sqrt(nor.x * nor.x + nor.z * nor.z);
+    else
+        v2 = glm::vec3(0, nor.z, -nor.y) / sqrt(nor.y * nor.y + nor.z * nor.z);
+    v3 = glm::cross(nor, v2);
+}
+
+inline __device__ glm::mat3 TangentSpaceToWorld(const glm::vec3 nor) {
+    glm::vec3 tan, bit;
+    buildTangentSpace(nor, tan, bit);
+    return glm::mat3(tan, bit, nor);
+}
+
+inline __device__ glm::mat3 WorldToTangentSpace(const glm::vec3 nor) {
+    return glm::transpose(TangentSpaceToWorld(nor));
+}
+
+inline __device__ glm::vec3 f_diffuse(glm::vec3 albedo) {
+    return albedo / PI;
+}
+
+inline __device__ glm::vec3 Sample_f_diffuse(glm::vec3 albedo, glm::vec2 xi, glm::vec3 nor,
+    glm::vec3& wiW, float& pdf) {
+    glm::vec3 wiLocal = squareToHemisphereCosine(xi);
+    pdf = wiLocal.z / PI;
+    glm::mat3 mat = TangentSpaceToWorld(nor);
+    wiW = mat * wiLocal;
+    return f_diffuse(albedo);
+}
 
 class GuiDataContainer
 {
